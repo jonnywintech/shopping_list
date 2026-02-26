3a0008b6e7f05cbffe9802c86e3e940947725030
Nikola Stankovic <jonnywintech@gmail.com>
2026-02-26 16:00:39 +0100

    added dropdown

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -29,6 +30,49 @@ class _NewItemState extends State<NewItem> {
                   }
                   return null;
                 },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(label: Text('Quantity')),
+                      initialValue: '1',
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: InputDecorator(
+                      decoration: InputDecoration(label: Text('Category')),
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        items: [
+                          for (final category in categories.entries)
+                            DropdownMenuItem(
+                              value: category.value,
+                              child: Row(
+                                children: [
+                                  Container(
+                                    width: 16,
+                                    height: 16,
+                                    color: category.value.color,
+                                  ),
+                                  SizedBox(
+                                    width: 6,
+                                  ),
+                                  Text(category.value.title),
+                                ],
+                              ),
+                            ),
+                        ],
+                        onChanged: (value) {},
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
