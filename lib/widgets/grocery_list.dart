87e2b78899baeec834e99124865b84e2e510eeb9
Nikola Stankovic <jonnywintech@gmail.com>
2026-03-02 13:51:53 +0100

    reverted to old version

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -14,59 +14,70 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  late Future<List<GroceryItem>> _loadedItems;
+
   var _isLoading = true;
   String? _error;
 
   @override
   void initState() {
     super.initState();
-    _loadedItems = _loadItems();
+    _loadItems();
   }
 
-  Future<List<GroceryItem>> _loadItems() async {
+  void _loadItems() async {
     final url = Uri.https(
       'flutter-prep-56665-default-rtdb.europe-west1.firebasedatabase.app',
       'shopping-list.json',
     );
-    final response = await http.get(url);
 
-    if (response.statusCode >= 400) {
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to load items. Please try again later.';
+          _isLoading = false;
+        });
+        return;
+      }
+
+      // depending of backend it gives different response when there is no data, so we need to check if it is null or not before parsing it
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+
+      final Map<String, dynamic> listData = json.decode(
+        response.body,
+      );
+      final List<GroceryItem> _loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+              (catItem) => catItem.value.title == item.value['category'],
+            )
+            .value;
+        _loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+      setState(() {
+        _groceryItems = _loadedItems;
+        _isLoading = false;
+      });
+    } catch (e) {
       setState(() {
         _error = 'Failed to load items. Please try again later.';
         _isLoading = false;
       });
-      return [];
     }
-
-    // depending of backend it gives different response when there is no data, so we need to check if it is null or not before parsing it
-    if (response.body == 'null') {
-      setState(() {
-        _isLoading = false;
-      });
-      return [];
-    }
-
-    final Map<String, dynamic> listData = json.decode(
-      response.body,
-    );
-    final List<GroceryItem> _loadedItems = [];
-    for (final item in listData.entries) {
-      final category = categories.entries
-          .firstWhere(
-            (catItem) => catItem.value.title == item.value['category'],
-          )
-          .value;
-      _loadedItems.add(
-        GroceryItem(
-          id: item.key,
-          name: item.value['name'],
-          quantity: item.value['quantity'],
-          category: category,
-        ),
-      );
-    }
-    return _loadedItems;
   }
 
   void _addItem() async {
@@ -162,80 +173,81 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
+    Widget _content = ListView.builder(
+      itemCount: _groceryItems.length,
+      itemBuilder: (ctx, index) {
+        return Dismissible(
+          key: ValueKey(_groceryItems[index].id),
+          background: Container(
+            decoration: BoxDecoration(
+              color: Theme.of(
+                context,
+              ).colorScheme.error.withValues(alpha: 0.75),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            margin: Theme.of(context).cardTheme.margin,
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+                Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart ||
+                direction == DismissDirection.startToEnd) {
+              _removeItem(_groceryItems[index]);
+            }
+          },
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        );
+      },
+    );
+
+    if (_groceryItems.isEmpty) {
+      _content = const Center(
+        child: Text('No items added yet.'),
+      );
+    }
+
+    if (_isLoading) {
+      _content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_error != null) {
+      _content = Center(
+        child: Text(_error!),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
       ),
       body: Center(
-        child: FutureBuilder(
-          future: _loadItems(),
-          builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Center(
-                child: const Text(
-                  'Failed to load items. Please try again later.',
-                ),
-              );
-            } else if (snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text('No items added yet.'),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (ctx, index) {
-                  return Dismissible(
-                    key: ValueKey(snapshot.data![index].id),
-                    background: Container(
-                      decoration: BoxDecoration(
-                        color: Theme.of(
-                          context,
-                        ).colorScheme.error.withValues(alpha: 0.75),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      margin: Theme.of(context).cardTheme.margin,
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.only(left: 20, right: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                          ),
-                          Icon(
-                            Icons.delete,
-                            color: Colors.white,
-                          ),
-                        ],
-                      ),
-                    ),
-                    onDismissed: (DismissDirection direction) {
-                      if (direction == DismissDirection.endToStart ||
-                          direction == DismissDirection.startToEnd) {
-                        _removeItem(snapshot.data![index]);
-                      }
-                    },
-                    child: ListTile(
-                      title: Text(snapshot.data![index].name),
-                      leading: Container(
-                        width: 24,
-                        height: 24,
-                        color: snapshot.data![index].category.color,
-                      ),
-                      trailing: Text(
-                        snapshot.data![index].quantity.toString(),
-                      ),
-                    ),
-                  );
-                },
-              );
-            }
-          },
-        ),
+        child: _content,
       ),
     );
   }
